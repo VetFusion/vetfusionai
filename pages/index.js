c6b2edd3fb6e4bcfa16631878bfb66631eb5e558
Ryan Whitney <ryanwhitney@Ryans-Mac-mini.local>
2025-03-19 14:26:25 -0700

    Deploying working /soap page

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>VetFusionAI is Live! 🚀</h1>
-      <p>Welcome to the future of AI-powered veterinary SOAP writing.</p>
-    </div>
-  );
-}
